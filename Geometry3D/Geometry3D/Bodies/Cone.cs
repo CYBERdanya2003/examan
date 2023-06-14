2532ad8628b5cf5be48e10cf19303c3bed7652f2
CYBERdanya2003 <danya.efimov0@yandex.ru>
2023-06-14 23:47:35 +0500

    exma2023

---
+++ Geometry3D/Geometry3D/Bodies/Cone.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry3D
+{
+    public class Cone : Body
+    {
+        public double Radius { get; }
+        public double SizeZ { get; }
+
+        public Cone(Vector3 position, double radius, double sizeZ) : base(position)
+        {
+            Radius = radius;
+            SizeZ = sizeZ;
+        }
+
+        public override bool ContainsPoint(Vector3 point)
+        {
+            var vectorX = point.X - Position.X;
+            var vectorY = point.Y - Position.Y;
+            var length2 = vectorX * vectorX + vectorY * vectorY;
+            var minZ = Position.Z - SizeZ / 2;
+            var maxZ = minZ + SizeZ;
+            if (point.Z < minZ || point.Z > maxZ)
+                return false;
+            var angle = Math.Atan2(Radius, SizeZ / 2);
+            var height = Math.Abs(point.Z - Position.Z);
+            var sectionRadius = height * Math.Tan(angle);
+            return length2 <= sectionRadius * sectionRadius;
+        }
+
+        public override RectangularCuboid GetBoundingBox()
+        {
+            var boundingBox = new RectangularCuboid(Position, 0, 0, 0);
+            boundingBox.SizeX = 2 * Radius;
+            boundingBox.SizeY = 2 * Radius;
+            boundingBox.SizeZ = SizeZ;
+            return boundingBox;
+        }
+    }
+}
---
+++ Geometry3D/Geometry3D/Body.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry3D
+{
+    public abstract class Body
+    {
+        public Vector3 Position { get; }
+
+        protected Body(Vector3 position)
+        {
+            Position = position;
+        }
+
+        public abstract bool ContainsPoint(Vector3 point);
+
+        public abstract RectangularCuboid GetBoundingBox();
+    }
+
+}
